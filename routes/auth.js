ffb2dff3349868950352aef3cc756a646c42fafe
diony0409@naver.com <diony0409@naver.com>
2023-08-18 14:56:41 +0900

    modified by diony0409

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
     CheckAuth: async function(req, res, next) {
-        console.log(req);
+            console.log(req);
         if(req.isAuthenticated()) {
             console.log('auth');
             next();
